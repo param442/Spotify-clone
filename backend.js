82d1cbb6bea7490f5faf85c94607f9826722e545
Paramvir SIngh <148181589+param442@users.noreply.github.com>
2024-01-21 22:55:04 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -7,7 +7,7 @@ const express = require("express");
 const { json } = require("body-parser");
 const app = express();
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+  res.header("Access-Control-Allow-Origin", "https://param442.github.io/Spotify-clone");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
